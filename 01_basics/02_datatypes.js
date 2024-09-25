295e95ebd00fcdeb6f66d6ffa97872b0f2a328d2
Varunrajsid <156994825+Varunrajsid@users.noreply.github.com>
2024-09-25 05:41:11 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer virsion code
+
+//  alert(3 +3) // we are using node.js not browser
+
+    console.log(3 
+    + 3) // code readeability should be high
+
+    console.log("hitesh")
+
+
+    let name = "hitesh"
+    let age = 18
+    let isLoggedIn = false
+    let state;
+
+
+    // ( These are the primitive datatypes)
+    // number => 2 to power of 53
+    // bigint 
+    // string => ""
+    // boolean = true/false
+    // null => standard value
+    // undefined =>
+    // symbol => unique
+
+
+    // objects
+
+    console.log(typeof name);
+    console.log(typeof null);// op = object
+    console.log(typeof undefined); //op = undefined
+    
